5e067d3dab733fe657e9d319fdeae3b4cbea25f0
Ahmet Seyhan <ahmetgcmf@outlook.com>
2024-03-26 14:36:16 +0300

    Areas Controller

--- Services/ProductManager.cs
+++ Services/ProductManager.cs
@@ -15,6 +15,23 @@ namespace Services
             _manager = manager;
         }
 
+        public void CreateProduct(Product product)
+        {
+            _manager.Product.Create(product);
+            _manager.Save();
+        }
+
+        public void DeleteOneProduct(int id)
+        {
+            Product product = GetOneProduct(id, false);
+            if(product != null)
+            {
+                _manager.Product.DeleteOneProduct(product);
+                _manager.Save();
+
+            }
+        }
+
         public IEnumerable<Product> GetAllProducts(bool trackChanges)
         {
             return _manager.Product.GetAllProducts(trackChanges);
@@ -29,6 +46,14 @@ namespace Services
                 throw new Exception("Product Not found!");
             }
             return product;
+        }
+
+        public void UpdateOneProduct(Product product)
+        {
+            var entity = _manager.Product.GetOneProduct(product.ProductId, true);
+            entity.ProductName = product.ProductName;
+            entity.Price = product.Price;
+            _manager.Save();
         }
     }
 }
