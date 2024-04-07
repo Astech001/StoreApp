b13efedd4377dc1dd8d1c8804936a125e6539c3d
Astech_Software <astech1610@gmail.com>
2024-04-07 21:22:59 +0300

    Tag Helpers

--- Entities/RequestParameters/RequestParameters.cs
+++ Entities/RequestParameters/RequestParameters.cs
@@ -5,8 +5,6 @@ namespace Entities.RequestParameters
     public abstract class RequestParameters
     {
         public String? SearchTerm { get; set; }
-        public int MinPrice { get; set; } = 0;
-        public int MaxPrice { get; set; } = int.MaxValue;
-        public bool IsValidPrice => MaxPrice > MinPrice;
+        
     }
 }
